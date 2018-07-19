4523a43b5b494b8767d69f90ff09f30ffec312dc
jackvid <jakovvidak2303@gmail.com>
2018-07-19 12:33:47 +0200

    Finished homework

--- TVShows/TVShows/Login/CreateAccountViewController.swift
+++ TVShows/TVShows/Login/CreateAccountViewController.swift
@@ -7,15 +7,39 @@
 //
 
 import UIKit
+import Alamofire
+import CodableAlamofire
+import SVProgressHUD
+
+
+struct User: Codable {
+    let email: String
+    let type: String
+    let id: String
+    enum CodingKeys: String, CodingKey {
+        case email
+        case type
+        case id = "_id"
+    }
+}
+
 
 class CreateAccountViewController: UIViewController {
 
+    //MARK: - Private -
     
-    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet private weak var emailTextField: UITextField!
     
-    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
     
     @IBOutlet weak var createButton: UIButton!
+    
+    @IBOutlet private weak var emailLabel: UILabel!
+    
+    @IBOutlet private weak var passwordLabel: UILabel!
+    
+    private var user : User?
+    //MARK - System -
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,20 +47,66 @@ class CreateAccountViewController: UIViewController {
         createButton.titleLabel?.textAlignment = NSTextAlignment.center
         emailTextField.setBottomBorder()
         passwordTextField.setBottomBorder()
+        emailLabel.isHidden = true
+        passwordLabel.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     
     // MARK: - Navigation
 
     @IBAction func createButtonClicked(_ sender: Any) {
-        if !((emailTextField.text?.isEmpty)!) && !((passwordTextField.text?.isEmpty)!) {
+        if (emailTextField.text?.isEmpty)! {
+            emailLabel.isHidden = false
+            emailLabel.text = "Set valid email adress"
+            return
+        }
+        else if (passwordTextField.text?.isEmpty)! {
+            passwordLabel.isHidden = false
+            passwordLabel.text = "Set valid password"
+            return
+        } else {
+        
+            let parameters: [String: String] = [
+                "email": emailTextField.text!,
+                "password": passwordTextField.text!
+            ]
+            
+            SVProgressHUD.show()
+            
+            Alamofire
+                .request("https://api.infinum.academy/api/users",
+                         method: .post,
+                         parameters: parameters,
+                         encoding: JSONEncoding.default)
+                .validate()
+                .responseDecodableObject(keyPath: "data", decoder: JSONDecoder()) {
+            (response: DataResponse<User>) in
+                    
+                    switch response.result {
+                    case .success(let user):
+                        self.user = user
+                        SVProgressHUD.setStatus("Success")
+                        print("Success: \(user)")
+                        SVProgressHUD.dismiss(withDelay: 1)
+                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                            let storyboard = UIStoryboard(name: "Login", bundle: nil)
+                            let viewControllerHome = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+                            self.navigationController?.pushViewController(viewControllerHome, animated: true)
+                        }
+                    case .failure(let error):
+                        SVProgressHUD.setStatus("ERROR")
+                        print("API \(error)")
+                        SVProgressHUD.dismiss(withDelay: 1)
+                        
+                    }
+            }
             
         }
+        
     }
     
 }
--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -8,16 +8,27 @@
 
 import UIKit
 import SVProgressHUD
+import Alamofire
+import CodableAlamofire
+
+struct LoginData: Codable {
+    let token: String
+}
 
 class LoginViewController: UIViewController {
     
     //MARK: - Private -
-    @IBOutlet private weak var rememberMeButton: UIButton!
+
+    @IBOutlet weak var rememberMeButton: UIButton!
     @IBOutlet private weak var emailTextField: UITextField!
     @IBOutlet private weak var logInButton: UIButton!
     @IBOutlet private weak var passwordTextField: UITextField!
     
     private var boolean = true
+    
+    private var loginData : LoginData?
+    
+    //MARK: - System -
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,15 +55,46 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction private func logInClick(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Home", bundle: nil)
-        let viewControllerHome = storyboard.instantiateViewController(withIdentifier: "HomeViewController")
-        navigationController?.pushViewController(viewControllerHome, animated: true)
+        
+        let parameters: [String: String] = [
+            "email": emailTextField.text!,
+            "password": passwordTextField.text!
+        ]
+        
+        SVProgressHUD.show()
+        
+        Alamofire
+            .request("https://api.infinum.academy/api/users/sessions",
+                     method: .post,
+                     parameters: parameters,
+                     encoding: JSONEncoding.default)
+            .validate()
+            .responseDecodableObject(keyPath: "data", decoder: JSONDecoder()) {
+                (response: DataResponse<LoginData>) in
+                
+                switch response.result {
+                case .success(let loginData):
+                    self.loginData = loginData
+                    SVProgressHUD.setStatus("Success")
+                    SVProgressHUD.dismiss(withDelay: 1)
+                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+                        let viewControllerHome = storyboard.instantiateViewController(withIdentifier: "HomeViewController")
+                        self.navigationController?.pushViewController(viewControllerHome, animated: true)
+                    }
+                case .failure(let error):
+                    SVProgressHUD.setStatus("ERROR")
+                    print("API \(error)")
+                    SVProgressHUD.dismiss(withDelay: 1)
+                }
+               
+        }
     }
     
     @IBAction private func createAnAccountClick(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Login", bundle: nil)
         let viewControllerHome = storyboard.instantiateViewController(withIdentifier: "CreateAccountViewController")
-        navigationController?.present(viewControllerHome, animated: true)
+        navigationController?.pushViewController(viewControllerHome, animated: true)
     }
     
 }
